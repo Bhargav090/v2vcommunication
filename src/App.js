b51eea1d4cb0e54ce9815266fb823c50bf306cd9
Bhargav Pathivada <bhargav.pathivada@onlyoncloud.com>
2024-11-18 14:37:40 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ const App = () => {
     <div className="app">
       <div className="road-container">
         <div className="road">
-          <h2>Road 1</h2>
+          <h2>RSU-1</h2>
           <div className="vehicles">
             {road1Vehicles.map((vehicle) => (
               <VehicleCard key={vehicle.vehicleID} vehicle={vehicle} />
@@ -69,7 +69,7 @@ const App = () => {
           </div>
         </div>
         <div className="road">
-          <h2>Road 2</h2>
+          <h2>RSU-2</h2>
           <div className="vehicles">
             {road2Vehicles.map((vehicle) => (
               <VehicleCard key={vehicle.vehicleID} vehicle={vehicle} />
